8cfd8fd51100196e22c55d1387ed49abc5f0e856
xieshuhao <xieshuhao@cloudstream-tech.com>
2022-06-20 15:49:48 +0800

    init, and test switch audio stream

---
+++ app/src/main/java/com/example/ijkplayersample/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.ijkplayersample;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+    private int playerIndex = 0;
+    private int playerType = 0;
+    private int checkedItem = 0;
+    private AlertDialog alertDialog;
+    Button btnPlay;
+    Button btnClear;
+    Button btnChoose;
+    EditText editText;
+    Switch hwSwitch;
+    RadioGroup rg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnPlay = findViewById(R.id.btn_play);
+        btnClear = findViewById(R.id.btn_clear);
+        //btnChoose = findViewById(R.id.btn_choose);
+        editText = findViewById(R.id.player_url);
+        hwSwitch = findViewById(R.id.hw_switch);
+        rg = findViewById(R.id.rgPlay);
+        //String path = Environment.getExternalStorageDirectory().getAbsolutePath();  //get the path
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.getCheckedRadioButtonId()) {
+                    case R.id.rb1:
+                        playerType = 0;
+                        break;
+                    case R.id.rb2:
+                        playerType = 1;
+                        break;
+                    case R.id.rb3:
+                        playerType = 2;
+                        break;
+                }
+            }
+        });
+
+        btnPlay.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, VideoActivity.class);
+
+                String videoUrl = editText.getText().toString();
+                boolean isEnableHw = false;
+                if(hwSwitch.isChecked() == true) {
+                    isEnableHw = true;
+                } else {
+                    isEnableHw = false;
+                }
+                Bundle bundle = new Bundle();
+                bundle.putString("videoUrl", videoUrl);
+                bundle.putBoolean("isEnableHw", isEnableHw);
+                bundle.putInt("playerType",playerType);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        } );
+        btnClear.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.getText().clear();
+            }
+        });
+    }
+
+    /*public void chooseVideoPlayer(View view) {
+        final String[] items = {"AndroidMediaPlayer", "ijkMediaPlayer", "IjkExoMediaPlayer"};
+        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle("Choose Player");
+        alertBuilder.setSingleChoiceItems(items, checkedItem, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                playerIndex = i;
+            }
+        });
+        alertBuilder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                playerType = playerIndex;
+                checkedItem = playerIndex;
+                switch (playerType) {
+                    case 0:
+                        Toast.makeText(getApplicationContext()," " + items[playerType],Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(getApplicationContext()," " + items[playerType],Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(getApplicationContext(), " " + items[playerType],Toast.LENGTH_SHORT).show();
+                        break;
+                    default :
+                        break;
+                }
+            }
+        });
+
+        alertBuilder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertDialog = alertBuilder.create();
+        alertDialog.show();
+    }*/
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
---
+++ app/src/main/java/com/example/ijkplayersample/VideoActivity.java
@@ -0,0 +1,142 @@
+package com.example.ijkplayersample;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.text.method.ScrollingMovementMethod;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import java.util.Map;
+
+import tv.danmaku.ijk.media.example.application.Settings;
+import tv.danmaku.ijk.media.example.widget.media.AndroidMediaController;
+import tv.danmaku.ijk.media.example.widget.media.IjkVideoView;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
+public class VideoActivity extends AppCompatActivity {
+    private AndroidMediaController mMediaController;
+    private IjkVideoView mVideoView;
+    private TableLayout mHudView;
+    private String mVideoPath;
+    private boolean mIsEnableHw;
+    private int mPlayerType = 0;
+    private Settings mSettings;
+    private Handler mHandler;
+    private Runnable mVolumeTask;
+    private ProgressBar mVolumeBar;
+    private TextView mTvLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,   //hide status bar
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_video);
+
+        getSupportActionBar().hide();
+
+        mSettings = new Settings(this);
+        mVolumeBar = findViewById(R.id.volume_bar);
+        updateUITask();
+        mTvLog = findViewById(R.id.tv_log);
+        mTvLog.setMovementMethod(ScrollingMovementMethod.getInstance());
+        mTvLog.setMaxLines(28);
+
+        mMediaController = new AndroidMediaController(this);
+
+        Bundle bundle = this.getIntent().getExtras();
+        mVideoPath = bundle.getString("videoUrl");
+        mIsEnableHw = bundle.getBoolean("isEnableHw");
+        mPlayerType = bundle.getInt("playerType");
+        initPlayer();
+
+        mHandler.postDelayed(mVolumeTask, 1000);
+        //Thread.currentThread().setPriority(2);  //error example
+    }
+
+    private void updateUITask() {
+        mHandler = new Handler();
+        mVolumeTask = new Runnable() {
+            @Override
+            public void run() {
+                mHandler.postDelayed(this,250);
+                showVolume();
+                scrollTextView();
+            }
+        };
+    }
+
+    private int getTextViewHeight(TextView view) {
+        Layout layout = view.getLayout();
+        int desired = layout.getLineTop(view.getLineCount());
+        int padding = view.getCompoundPaddingTop() + view.getCompoundPaddingBottom();
+        return desired + padding;
+    }
+
+    private void scrollTextView() {
+        int offset = getTextViewHeight(mTvLog);
+        //int offset = mTvLog.getLineCount() * mTvLog.getLineHeight();
+        if(offset > mTvLog.getHeight()) {
+            mTvLog.scrollTo(0, offset - mTvLog.getHeight());
+        }
+    }
+
+    private void showVolume() {
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        double currentVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        double maxVolume = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        mVolumeBar.setProgress((int)(currentVolume/maxVolume * mVolumeBar.getMax()));
+    }
+
+    private void initPlayer() {
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        //IjkMediaPlayer.native_profileBegin("libijkplayer.so");
+
+        mVideoView = findViewById(R.id.v_view);
+        mHudView = findViewById(R.id.hud_view);
+
+        mVideoView.setTextView(mTvLog);
+        mVideoView.setMediaController(mMediaController);
+        //MediaMetadataRetriever mmr = new MediaMetadataRetriever();  //test
+        //mmr.setDataSource(mVideoPath, new HashMap<String, String>());
+        MediaMetadataRetriever mmr = null;
+        mVideoView.setHudView(mHudView, mmr);
+        mVideoView.setVideoURI(Uri.parse(mVideoPath));
+        mVideoView.setEnableHw(mIsEnableHw);
+        mVideoView.setPlayerType(mPlayerType);
+        mVideoView.start();
+        //for test
+        //Map<Thread, StackTraceElement[]> allStackTraces = Thread.getAllStackTraces();
+        //Log.d("test", "线程总数：" + allStackTraces.size());
+        //error example
+        /*Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mVideoView.setVideoURI(Uri.parse(mVideoPath));
+                mVideoView.setEnableHw(mIsEnableHw);
+                mVideoView.setPlayerType(mPlayerType);
+                mVideoView.start();
+            }
+        });
+        t1.start();
+        t1.setPriority(1);*/
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mVideoView != null) {
+            mVideoView.stopPlayback();
+            mVideoView.release(true);
+            mVideoView.stopBackgroundPlay();
+        }
+        //IjkMediaPlayer.native_profileEnd();
+    }
+}
